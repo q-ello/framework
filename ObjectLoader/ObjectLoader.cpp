1fa394063e48711abf6c8ba049881a976fe2d803
juliena <yulka.polutova@mail.ru>
2025-02-22 02:57:36 +0300

    miniUnreal with transform

---
+++ ObjectLoader/ObjectLoader.cpp
@@ -0,0 +1,32 @@
+//***************************************************************************************
+// CrateApp.cpp by Frank Luna (C) 2015 All Rights Reserved.
+//***************************************************************************************
+#include "MyApp.h"
+
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+
+const int gNumFrameResources = 3;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+	PSTR cmdLine, int showCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	try
+	{
+		MyApp theApp(hInstance);
+		if (!theApp.Initialize())
+			return 0;
+
+		return theApp.Run();
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
+}
