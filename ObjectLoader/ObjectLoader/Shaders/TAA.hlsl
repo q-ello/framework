15423d63b3af383b69a2ad03dc746ffd06d2f95e
Juliena Polutova <yulka.polutova@mail.ru>
2025-12-13 00:09:45 +0300

    something changed in TAA but I'm not sure what

--- ObjectLoader/ObjectLoader/Shaders/TAA.hlsl
+++ ObjectLoader/ObjectLoader/Shaders/TAA.hlsl
@@ -20,8 +20,6 @@ SamplerComparisonState gsamShadow : register(s0);
 SamplerState gsamLinear : register(s1);
 SamplerState gsamLinearWrap : register(s2);
 
-static const float depthThreshold = 0.5f;
-
 float4 TAAPS(VertexOut pin) : SV_Target
 {
     int3 currCoords = int3(pin.PosH.xyz);
@@ -39,7 +37,7 @@ float4 TAAPS(VertexOut pin) : SV_Target
     float4 prevViewPos = mul(world, gPrevView);
     float4 prevScreenNDC = mul(prevViewPos, gPrevProj);
     prevScreenNDC /= prevScreenNDC.w;
-    float2 prevUV = float2(prevScreenNDC.x * 0.5f + 0.5f, 0.5f - prevScreenNDC.y * 0.5f);
+    float2 prevUV = float2(prevScreenNDC.x * 0.5f + 0.5f, 0.5f - prevScreenNDC.y * 0.5f);   
 
     if (prevUV.x < 0 || prevUV.x > 1 || prevUV.y < 0 || prevUV.y > 1)
         return currentColor;
@@ -48,21 +46,51 @@ float4 TAAPS(VertexOut pin) : SV_Target
 
     int3 prevCoords = int3(prevUV * gScreenSize, 0);
 
-    float4 actuaPrevClipPos = float4(prevScreenNDC.x, prevScreenNDC.y, gPreviousDepth.Load(prevCoords).r, 1.0f);
+    float prevDepthNDC = gPreviousDepth.Load(prevCoords).r;
+
+    float4 actuaPrevClipPos = float4(prevScreenNDC.x, prevScreenNDC.y, prevDepthNDC, 1.0f);
     float4 actualPrevViewPos = mul(actuaPrevClipPos, gPrevInvProj);
     actualPrevViewPos /= actualPrevViewPos.w;
 
     float actualPrevViewDepth = actualPrevViewPos.z;
 
-    float bias = max(predictedPrevViewDepth * 0.01f, 0.02f);
-
-    float d = abs(predictedPrevViewDepth - actualPrevViewDepth);
-    float t = saturate(d / bias); // 0..1
-    float blend = lerp(0.0f, 0.9f, saturate(1.0f - t)); // more depth diff → less history
+    float depthDiff = abs(predictedPrevViewDepth - actualPrevViewDepth);
+    float depthFactor = saturate(1.0f - depthDiff / predictedPrevViewDepth);  
 
     float4 historyColor = gHistory.Load(prevCoords);
     
-    output = lerp(currentColor, historyColor, blend);
-    
+    if (depthFactor == 0.0f)
+    {
+        float4 minC = 1e9, maxC = -1e9;
+        float minDepth = -1e9, maxDepth = -1e9;
+        static const int2 offs[4] = {int2(1,0), int2(-1,0), int2(0,1), int2(0,-1)};
+        for(int i=0;i<4;i++){
+            int3 neighbourCoords = int3(currCoords + offs[i], 0);
+            float neighbourDepth = gCurrentDepth.Load(neighbourCoords).r;
+            minDepth = min(minDepth, neighbourDepth);
+            maxDepth = max(maxDepth, neighbourDepth);
+            if (abs(neighbourDepth - depth) > 0.05)
+                continue;
+
+            minC = min(minC, neighbourDepth);
+            maxC = max(maxC, neighbourDepth);
+        }
+        float depthSlope = maxDepth - minDepth;
+        float tolerance = max(0.002, depthSlope * 2.0);
+
+        bool badHistory = abs(prevDepthNDC - depth) > tolerance;
+
+        if (badHistory)
+            return currentColor;
+
+        depthFactor = 0.9f;
+        historyColor = clamp(historyColor, minC, maxC);
+    }
+
+    float histWeight = 0.9f * depthFactor;
+    float3 finalColor = lerp(currentColor, historyColor, histWeight);
+
+    output = float4(finalColor, 1.0f);
+
     return output;
 }
